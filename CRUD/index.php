512060ed32f67c0d4c96e9a99c9d8e2c4a775c58
Korvo2irow <82429011+Korvo2Tuirow@users.noreply.github.com>
2023-07-08 18:30:18 -0300

    crud

---
+++ CRUD/index.php
@@ -0,0 +1,30 @@
+<?php 
+$host = 'localhost';
+$usuario = 'root';
+$senha = '';
+$database = 'cadastro';
+
+$connect = mysqli_connect($host, $usuario, $senha, $database);
+
+$sql = "SELECT * FROM gafanhotos";
+
+$query = mysqli_query($connect, $sql);
+
+echo "<pre>";
+
+while($resultado = mysqli_fetch_array($query)){
+    echo"id = ". $resultado[0]."<br>". $resultado[1]."<br>".$resultado[2]."<br>". $resultado[3]."<br>". $resultado[4]."<br>". $resultado[5]."<br>". $resultado[6]."<br>". $resultado[7]."<br>". $resultado[8]."<br> <br>";
+};
+
+//print_r($resultado);
+
+
+
+
+
+
+echo "</pre>";
+
+
+
+?>
