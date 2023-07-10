14f23406af57000c282ab21880c033df3f90706a
Korvo2irow <82429011+Korvo2Tuirow@users.noreply.github.com>
2023-07-10 11:33:32 -0300

    crud

--- CRUD/index.php
+++ CRUD/index.php
@@ -9,18 +9,23 @@ $connect = mysqli_connect($host, $usuario, $senha, $database);
 $MSG = ($connect)? "CONECTADO" : "ERRO";
 echo $MSG;
 
-$sql = "SELECT * FROM gafanhotos";
+$sql = "SELECT nome, profissao FROM gafanhotos where id like 15";
 
 $query = mysqli_query($connect, $sql);
 
+
+
+
 echo "<pre>";
 
-while($resultado = mysqli_fetch_array($query)){
+/*while($resultado = mysqli_fetch_array($query)){
     echo"id = ". $resultado[0]."<br>". $resultado[1]."<br>".$resultado[2]."<br>". $resultado[3]."<br>". $resultado[4]."<br>". $resultado[5]."<br>". $resultado[6]."<br>". $resultado[7]."<br>". $resultado[8]."<br> <br>";
-};
+};*/
 
 
-//print_r($resultado);
+$resultado = mysqli_fetch_all($query);
+    print_r ($resultado);
+
 
 echo "</pre>";
 
