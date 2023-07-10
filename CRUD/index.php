de01561e0ab4105369a87e93dcea1536b5f96c61
Korvo2irow <82429011+Korvo2Tuirow@users.noreply.github.com>
2023-07-10 10:44:45 -0300

    crud

--- CRUD/index.php
+++ CRUD/index.php
@@ -6,6 +6,9 @@ $database = 'cadastro';
 
 $connect = mysqli_connect($host, $usuario, $senha, $database);
 
+$MSG = ($connect)? "CONECTADO" : "ERRO";
+echo $MSG;
+
 $sql = "SELECT * FROM gafanhotos";
 
 $query = mysqli_query($connect, $sql);
@@ -15,6 +18,7 @@ echo "<pre>";
 while($resultado = mysqli_fetch_array($query)){
     echo"id = ". $resultado[0]."<br>". $resultado[1]."<br>".$resultado[2]."<br>". $resultado[3]."<br>". $resultado[4]."<br>". $resultado[5]."<br>". $resultado[6]."<br>". $resultado[7]."<br>". $resultado[8]."<br> <br>";
 };
+
 
 //print_r($resultado);
 
