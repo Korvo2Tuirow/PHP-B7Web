feb1d4c6abe925f0c39ab3be7c51bb916f008616
Korvo2irow <82429011+Korvo2Tuirow@users.noreply.github.com>
2023-07-10 01:29:26 -0300

    switch

---
+++ switch.php
@@ -0,0 +1,18 @@
+<?php
+
+$nome = "korvo";
+
+switch($nome){
+    case "JJ": echo "JJ";
+    break;
+
+    case "Luke": echo "Luke";
+    break;
+
+    case "korvo": echo "korvo";
+    break;
+
+    default: echo "ERRO";
+};
+
+?>
