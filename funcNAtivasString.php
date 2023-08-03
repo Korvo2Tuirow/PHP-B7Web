e0dfca14c4e2fc6b13daec4b88f642edde864723
Korvo2irow <82429011+Korvo2Tuirow@users.noreply.github.com>
2023-08-03 16:45:42 -0300

    fnstring

--- funcNAtivasString.php
+++ funcNAtivasString.php
@@ -15,34 +15,51 @@ $nomeAlterado = str_replace("Korvo", "Robert",$nome);
 echo $nomeAlterado."<br>";// substitui um pelo outro(pode ser apenas letras tb)
 
 $parteNome = substr($nome, 2 , 7);
-echo $parteNome; // pega 7 caracteres apartir da segunda 
+echo $parteNome."<br>"; // pega 7 caracteres apartir da segunda 
 
+//Posição do caractere na string
+$nomeCompleto = "Korvo Tuirow";
+$posicao = strpos($nomeCompleto, "rvo");
+echo $posicao."<br>";
+if($posicao > -1){
+    echo "Achou<br>";
+}else{
+    echo "não contem<br>";
+};
 
+$nome = "robert";
+echo ucfirst($nome)."<br>"; //Deixa a primeira letra em maiuscula
 
+$nomeCompleto = "robert lopes ramos";
+echo ucwords($nomeCompleto)."<br>";//Primeira letra em maiuscula de todas as palavras
 
+$arrayNome = explode(" ", $nomeCompleto);// transforma em arrays
+print_r($arrayNome); echo"<br>";//Primeira letra em maiuscula
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+$numero = 105632.755;
+echo number_format($numero, 2, ',', '.');
 
 
 
 ?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
