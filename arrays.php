3f8a68ea23f1bf691f255a37763880e07f8d61db
Korvo2irow <82429011+Korvo2Tuirow@users.noreply.github.com>
2023-09-05 17:26:18 -0300

    arrays

--- arrays.php
+++ arrays.php
@@ -6,6 +6,7 @@ foreach($array as $_key){
     echo $_key."<br>";
 }
 
+echo "<hr>";
 //////////////////////////////////////////////////////////
 
 //KEY_EXISTS
@@ -20,11 +21,91 @@ $dados = [
 
 if(key_exists('idade', $dados)){
     $idade = $dados['idade'];
-    echo $idade;
+    echo "A idade é ".$idade;
 }else{
     echo"Idade não encontrada";
 }
 
+echo "<hr>";
 ///////////////////////////////////////////////////////////////
+
+
+$chaves = array_keys($dados);
+
+echo"<pre>";
+
+print_r($dados);
+
+print_r ($chaves);
+
+echo "<br>";
+
+$valores = array_values($dados);
+print_r($valores);
+
+echo "<hr>";
+
+///////////////////////////////////////////////////////////////
+
+
+
+?>
+
+<table border="1">
+
+    <?php foreach($dados as $chaves => $valores):?>
+        <tr>
+            <th><?php echo $chaves;?></th>
+            <td><?php echo $valores;?></td>
+        </tr>
+
+
+    <?php  endforeach?>
+
+</table>
+
+<hr>
+
+<table border="1">
+<?php 
+echo "<tr>";
+foreach(array_keys($dados) as $chave){
+echo"
+<th>".$chave."</th>";};
+echo "</tr>";
+echo "<tr>";
+foreach(array_values($dados) as $valor){
+    echo"
+    <td>".$valor."</td>";};
+    echo "</tr>";
+?>
+</table>
+
+<hr/>
+
+<?php 
+
+//////////////////////////////////////////////////////////
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 ?>
